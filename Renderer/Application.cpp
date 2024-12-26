7240bbae2bf1b45fe24a757638f4e8e084604e13
Oikmo <genisis344@gmail.com>
2024-12-26 00:21:51 +0000

    Well kol, maybe if you weren't showing your ass on screen whilst teaching trigonomtry and vertices, I just implemented modern opengl. HA!

--- Renderer/Application.cpp
+++ Renderer/Application.cpp
@@ -1,12 +1,16 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
+#include <cmath>
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "Failed to load GLFW!\n";
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,17 +23,31 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(glewInit() != GLEW_OK) {
+        std::cout << "Failed to load GLEW!\n";
+        return -1;
+    }
+
+    float positions[6] = {
+        -0.5, -0.5,
+         0.0,  0.5,
+         0.5, -0.5
+    };
+
+    std::cout << glGetString(GL_VERSION) << "\n";
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.0, 0.5);
-        glVertex2f(0.5, -0.5);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
