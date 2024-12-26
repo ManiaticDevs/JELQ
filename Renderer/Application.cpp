5f7234003b9aebacd24bdb19a92fcea0e7f8594b
LYCNK <91485463+LYCNK@users.noreply.github.com>
2024-12-26 00:56:23 +0000

    reverted prev commit.

--- Renderer/Application.cpp
+++ Renderer/Application.cpp
@@ -1,16 +1,12 @@
-#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <cmath>
+
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit()) {
-        std::cout << "Failed to load GLFW!\n";
+    if (!glfwInit())
         return -1;
-    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -23,31 +19,11 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    if(glewInit() != GLEW_OK) {
-        std::cout << "Failed to load GLEW!\n";
-        return -1;
-    }
-
-    float positions[6] = {
-        -0.5, -0.5,
-         0.0,  0.5,
-         0.5, -0.5
-    };
-
-    std::cout << glGetString(GL_VERSION) << "\n";
-
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
